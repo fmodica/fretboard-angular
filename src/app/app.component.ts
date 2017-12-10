88f8c453711339d4416d4a0908190b16ffcd14f8
Frank Modica <fmodica85@gmail.com>
2017-12-10 13:14:57 -0500

    Moved the fretboard module to the root folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Interfaces } from './fretboard-module/fretboard.module';
+import { Interfaces } from '../../fretboard-module/fretboard.module';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { FretboardModule } from './fretboard-module/fretboard.module';
+import { FretboardModule } from '../../fretboard-module/fretboard.module';
 
 import { AppComponent } from './app.component';
 
